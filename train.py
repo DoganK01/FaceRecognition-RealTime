891d844cdc1446e25ba030fceb028c3b6a54944e
DoganK01 <98788987+DoganK01@users.noreply.github.com>
2022-06-04 11:53:39 +0300

    Update train.py

--- train.py
+++ train.py
@@ -90,7 +90,7 @@ model.add(Conv2D(filters=16, kernel_size=(3, 3), activation="relu", padding="sam
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 model.add(Flatten())
-model.add(Dense(units=256, activation="relu"))
+model.add(Dense(units=32, activation="relu"))
 model.add(Dropout(0.2))
 model.add(Dense(units=no0fClasses, activation="sigmoid"))
 
